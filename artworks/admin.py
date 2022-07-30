2678787a73e1749ae3a32aa1aa6a19d21ab579e4
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-07-30 19:24:31 +0000

    feat: Add model for Artwork and upload data via fixtures

--- artworks/admin.py
+++ artworks/admin.py
@@ -1,7 +1,7 @@
 """ Admin config and for artworks tables """
 
 from django.contrib import admin
-from .models import Artist, ArtGenre, ArtStyle
+from .models import Artist, ArtGenre, ArtStyle, Artwork
 
 
 @admin.register(Artist)
@@ -23,3 +23,9 @@ class ArtStyleAdmin(admin.ModelAdmin):
     ''' Maintain ArtStyle '''
     model = ArtStyle
     list_display = ('pk', 'name', 'friendly_name', )
+
+
+@admin.register(Artwork)
+class ArtworkAdmin(admin.ModelAdmin):
+    ordering = ('sku',)
+    list_display = ('sku', 'name', 'on_sale', 'sale_percentage')
--- artworks/models.py
+++ artworks/models.py
@@ -1,6 +1,7 @@
 """ Artwork and related tables model """
 
 from django.db import models
+from utility.models import SystemPreference
 
 
 class Artist(models.Model):
@@ -55,3 +56,37 @@ class ArtGenre(models.Model):
     def get_friendly_name(self):
         """ Get the friendly name for display"""
         return self.friendly_name
+
+
+class Artwork(models.Model):
+    """ Artwork to be selected by user """
+
+    sku = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    artist = models.ForeignKey('Artist', null=True, blank=True,
+                               on_delete=models.SET_NULL)
+    genre = models.ForeignKey('ArtGenre', null=True, blank=True,
+                              on_delete=models.SET_NULL)
+    style = models.ForeignKey('ArtStyle', null=True, blank=True,
+                              on_delete=models.SET_NULL)
+    on_sale = models.BooleanField(default=False, null=True, blank=True)
+    sale_percentage = models.ForeignKey(SystemPreference, null=True,
+                                        blank=True, on_delete=models.SET_NULL,
+                                        default="S")
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    rating = models.DecimalField(max_digits=5, decimal_places=2,
+                                 null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_sale_price(self):
+        """ Calculate and return sale price from settings
+        in the System Preference """
+        if self.sale_percentage.data:
+            sales_per = int(self.sale_percentage.data)
+            return self.price * sales_per/100
+        else:
+            return self.price
