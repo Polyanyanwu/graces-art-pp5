e243723e6c01cfc92b548e9be49d8ed02041c47e
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-08-01 17:44:05 +0000

    style: Update artwork form to display sorted friendly names for dropdowns

--- artworks/forms.py
+++ artworks/forms.py
@@ -36,3 +36,29 @@ class ArtworkForm(forms.ModelForm):
         model = Artwork
         fields = ('id', 'name', 'artist', 'genre', 'style', 'sku', 'on_sale',
                   'price', 'rating', 'image_url', 'image')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # display friendly names for artists dropdown sorted
+        artists = Artist.objects.all()
+        friendly_names = [(a.id, a.get_friendly_name()) for a in artists]
+        friendly_names.append(('--', '-'))
+        sorted_friendly_names = sorted((trans for trans in friendly_names),
+                                       key=lambda x: x[1])
+        self.fields['artist'].choices = sorted_friendly_names
+
+        # display friendly names for Genre dropdown
+        genres = ArtGenre.objects.all()
+        friendly_names = [(g.id, g.get_friendly_name()) for g in genres]
+        friendly_names.append(('--', '-'))
+        sorted_friendly_names = sorted((trans for trans in friendly_names),
+                                       key=lambda x: x[1])
+        self.fields['genre'].choices = sorted_friendly_names
+
+        # display friendly names for Style dropdown
+        styles = ArtStyle.objects.all()
+        friendly_names = [(s.id, s.get_friendly_name()) for s in styles]
+        friendly_names.append(('--', '-'))
+        sorted_friendly_names = sorted((trans for trans in friendly_names),
+                                       key=lambda x: x[1])
+        self.fields['style'].choices = sorted_friendly_names
