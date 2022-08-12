69021bc6671df4fc7605e4923221dc734a0738a4
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-08-12 08:29:55 +0000

    feat: Add model for wishlist

--- bag/models.py
+++ bag/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+""" Models for the bag app"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from artworks.models import Artwork
+
+
+class WishList(models.Model):
+    """ Model to maintain wishlist for logged in users """
+    artwork = models.ForeignKey(Artwork, on_delete=models.SET_NULL,
+                                null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL,
+                             null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.artwork.name
