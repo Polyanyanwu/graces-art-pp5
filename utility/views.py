165018cabf7a5685b73d5e2b8570414e4115861a
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-08-13 14:02:17 +0000

    fix: System preference integer type accepting string

--- utility/views.py
+++ utility/views.py
@@ -1,6 +1,6 @@
 """ Utility Tables Maintenance View """
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.db.utils import DataError
 from django.core.exceptions import ObjectDoesNotExist
@@ -34,20 +34,24 @@ class SystemPreferenceView(View):
 
         if 'save_system_preference' in request.POST:  # save button clicked
             selected_item = request.POST.get('pref_code')
-            pref_form = get_object_or_404(SystemPreference, code=selected_item)
-            data = int(request.POST.get('data'))
-            pref_form.data = data
+            pref = get_object_or_404(SystemPreference, code=selected_item)
+            data_type = pref.data_type
+            data = request.POST.get('data')
+
+            if data_type == 'int':
+                try:
+                    data_int = int(data)
+                except ValueError:
+                    messages.error(request, 'Only integer values are allowed')
+                    return redirect('system_preference')
+            pref.data = data
             try:
-                pref_form.save()
-                messages.add_message(request, messages.INFO,
-                                     'Data updated successfully')
+                pref.save()
+                messages.info(request, 'Data updated successfully')
             except DataError:
                 print(" Data error saving system preference ")
-                messages.add_message(request, messages.INFO,
-                                     'Error saving cancellation, try later')
-            syspref = SystemPreference.objects.all()
-            form = SystemPreferenceForm(instance=syspref[0])
-            selected_item = syspref[0].code
+                messages.error(request, 'Error saving cancellation, try later')
+            form = SystemPreferenceForm(instance=pref)
         else:
             selected_item = request.POST.get('pref_code')
             syspref = SystemPreference.objects.all()
@@ -60,7 +64,7 @@ class SystemPreferenceView(View):
                 form = SystemPreferenceForm(instance=curr)
             else:
                 form = SystemPreferenceForm(instance=syspref[0])
-
+        syspref = SystemPreference.objects.all()
         return render(
             request,
             "utility/sys_pref/sys_pref.html",
