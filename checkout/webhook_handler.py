fa088850ddbfbfa6bdac1b12cf9513462c06158c
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-08-14 16:06:15 +0000

    fix: Webhook handler sends email twice indentation issue

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -186,11 +186,11 @@ class StripeWebhookHandler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
-        self._send_email_confirmation(order)
-        self._write_notification(order)
-        return HttpResponse(
-            content=f'Webhook received {event["type"]} | \
-                      SUCCESS: Created in webhook', status=200)
+            self._send_email_confirmation(order)
+            self._write_notification(order)
+            return HttpResponse(
+                content=f'Webhook received {event["type"]} | \
+                        SUCCESS: Created in webhook', status=200)
 
     def handle_payment_intent_payment_failed(self, event):
         """ Handle the payment_intent.payment_failed webhook from stripe """
